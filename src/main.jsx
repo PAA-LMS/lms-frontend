72d7055756bbec24c55389fdc0a7ef1a12a01ba2
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2025-02-19 15:35:49 +0530

    updated the main.jsx file and etc

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx';
-import SignupPage from './pages/signupPage/signup.jsx'
-import LoginPage from './pages/loginPage/login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
